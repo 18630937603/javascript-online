a6021aa30c4ba082e2f88a51aabbb44afa10a49c
lzh <18630937603@163.com>
2021-08-25 17:13:52 +0800

    2021年8月25日17:13:47

---
+++ test.js
@@ -0,0 +1,9 @@
+function createSingleton(value) {
+    let instance = null
+    return function (){
+        if(this.instance){
+
+        }
+        return instance
+    }
+}
---
+++ thisTest.js
@@ -0,0 +1,23 @@
+let a = function (){
+    console.log(this)
+}
+
+class B{
+    constructor() {
+        console.log(this)
+    }
+    getThis(){
+        console.log(this)
+    }
+}
+
+let c = {
+    fn:function (){
+        console.log(this)
+    }
+}
+
+a()
+let b = new B()
+b.getThis()
+c.fn()
