fb0963b8bac17563183eef2ba39f1f6091c92862
lzh <18630937603@163.com>
2021-08-26 23:01:55 +0800

    2021年8月26日23:01:53

--- 215. 数组中的第K个最大元素 medium quickSort.js
+++ 215. 数组中的第K个最大元素 medium quickSort.js
@@ -30,4 +30,3 @@
 // };
 //
 // console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
-
--- 剑指 Offer 19. 正则表达式匹配.js
+++ 剑指 Offer 19. 正则表达式匹配.js
@@ -1,5 +1,5 @@
-"use strict";
-// function isMatch(s: string, p: string): boolean {
+// "use strict";
+// function isMatch(s, p){
 //     if(p.length===0){return s.length===0}
 //     let first_match = !(s.length===0) && (s[0]===p[0] || p[0] === '.')
 //     if(p.length>=2 && p[1]==='*'){
@@ -8,4 +8,3 @@
 //         return first_match && isMatch(s.substr(1), p.substr(1));
 //     }
 // }
-//# sourceMappingURL=%E5%89%91%E6%8C%87%20Offer%2019.%20%E6%AD%A3%E5%88%99%E8%A1%A8%E8%BE%BE%E5%BC%8F%E5%8C%B9%E9%85%8D.js.map
